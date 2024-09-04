a1e5c4d7b1fa5f4a8be0a891ea742da4fb23109f
A. Legayada <tonilegayada@gmail.com>
2024-09-04 23:18:36 +0800

    completed base tasks
    -the gui could be improved
    - the elements could be more organized
    - the code could be furhur refactored
    - should add back the comments

--- public/dist/js/script.js
+++ public/dist/js/script.js
@@ -1,39 +1,80 @@
 //task tracker
-document.addEventListener('DOMContentLoaded', function (e) { return getItems(); });
+document.addEventListener('DOMContentLoaded', function (e) {
+    displayItems();
+});
 var trackerContent = document.querySelector('#taskTracker');
+var listItemCollection = [];
 var addbutton = document.querySelector('#addTaskButton');
 var addInput = document.querySelector('#addTaskInput');
 var datePicker = document.querySelector('#expire-date');
 var actualList = document.querySelector('#TaskulParent');
 var SomethingWentWrong = document.querySelector('#liveAlertPlaceholder');
+var sortSelect = document.querySelector('#sort-select');
 var taskTracker = document.querySelector('#taskTracker');
 taskTracker.textContent = "".concat(getActiveTask(), " / ").concat(getTasks(), " Completed!");
-// //I guess this code is useful if you have like data from a jsonfile or a
-// //local storage but tbh if you're just creating everything from scratch this isn't
-// //even needed
-// const checkButton: NodeListOf<HTMLLIElement> =
-//   document.querySelectorAll('.fa-check-circle')
-// const trashButton: NodeListOf<HTMLLIElement> =
-//   document.querySelectorAll('.fa-trash ')
-// checkButton.forEach((btn) => {
-//   btn.addEventListener('click', checkedMark)
-// })
-// trashButton.forEach((btn) => {
-//   btn.addEventListener('click', trashedF)
-// })
-// //up to here
+sortSelect.addEventListener('change', sortit);
 addbutton.addEventListener('click', addTask);
 function reset() {
     addInput.value = '';
-    // console.log(checkButton)
+    datePicker.value = '';
 }
-function getItems() {
-    // the display function
+function sortit() {
+    var items = document.querySelectorAll('.list-group-item');
+    if (sortSelect.value === 'completed') {
+        items.forEach(function (item) {
+            if (item.classList.contains('active')) {
+                item.classList.add('hide');
+            }
+            else {
+                item.classList.remove('hide');
+            }
+        });
+    }
+    else if (sortSelect.value === 'pending') {
+        items.forEach(function (item) {
+            if (item.classList.contains('inactive')) {
+                item.classList.add('hide');
+            }
+            else {
+                item.classList.remove('hide');
+            }
+        });
+    }
+    else {
+        items.forEach(function (item) {
+            if (item.classList.contains('hide')) {
+                item.classList.remove('hide');
+            }
+        });
+    }
+    setTracker();
+}
+function compareDates(date) {
+    var compareDate = new Date(date);
+    var current = new Date();
+    if (compareDate < current) {
+        return 'expired ';
+    }
+    else {
+        return 'nonexpired';
+    }
+}
+function displayItems() {
+    actualList.innerHTML = '';
     var tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.sort(function (a, b) { return a.value.localeCompare(b.value); });
     console.log(tasks);
     tasks.forEach(function (task) {
         var newLi = document.createElement('li');
-        newLi.setAttribute('class', 'list-group-item active shadow');
+        newLi.setAttribute('class', 'list-group-item shadow');
+        if (task.completed) {
+            newLi.classList.add('inactive');
+        }
+        else {
+            newLi.classList.add('active');
+        }
+        newLi.setAttribute('id', "".concat(tasks.indexOf(task)));
+        newLi.setAttribute('name', "".concat(task.value));
         var checked = document.createElement('i');
         checked.setAttribute('class', 'fas fa-check-circle fa-lg');
         var trashed = document.createElement('i');
@@ -42,31 +83,16 @@ function getItems() {
         var spanned = document.createElement('span');
         spanned.innerText = "".concat(task.value);
         spanned.setAttribute('class', 'content');
+        expirespan.innerText = " Due ".concat(task.expiry);
+        expirespan.setAttribute('class', compareDates(task.expiry));
         checked.addEventListener('click', checkedMark);
         trashed.addEventListener('click', trashedF);
         newLi.appendChild(checked);
         newLi.appendChild(trashed);
+        newLi.appendChild(expirespan);
         newLi.appendChild(spanned);
         actualList.appendChild(newLi);
     });
-    // const newLi = document.createElement('li')
-    // newLi.setAttribute('class', 'list-group-item')
-    // //list-group-item active shadow"
-    // newLi.classList.add('active')
-    // newLi.classList.add('shadow')
-    // const checked = document.createElement('i')
-    // checked.setAttribute('class', 'fas fa-check-circle fa-lg')
-    // const trashed = document.createElement('i')
-    // trashed.setAttribute('class', 'fa-solid fa-trash fa-lg')
-    // const spanned = document.createElement('span')
-    // spanned.innerText = `${addInput.value}`
-    // spanned.setAttribute('class', 'content')
-    // checked.addEventListener('click', checkedMark)
-    // trashed.addEventListener('click', trashedF)
-    // newLi.appendChild(checked)
-    // newLi.appendChild(trashed)
-    // newLi.appendChild(spanned)
-    // actualList.appendChild(newLi)
 }
 function getTasks() {
     var count = 0;
@@ -82,64 +108,74 @@ function getActiveTask() {
     });
     return count;
 }
+function throwError(error) {
+    SomethingWentWrong.innerText = error;
+    SomethingWentWrong.style.display = 'block';
+    setTimeout(function (e) {
+        SomethingWentWrong.style.display = 'none';
+    }, 2000);
+}
 function setTracker() {
     taskTracker.textContent = "".concat(getActiveTask(), " / ").concat(getTasks(), " Completed!");
-    // console.log(actualList.children)
 }
 function addTask(e) {
     e.preventDefault();
-    if (addInput.value.length < 1) {
-        SomethingWentWrong.innerText = 'Invalid String Input!';
-        SomethingWentWrong.style.display = 'block';
-        setTimeout(function (e) {
-            SomethingWentWrong.style.display = 'none';
-        }, 2000);
+    if (addInput.value.trim().length < 1) {
+        throwError('Invalid String input');
     }
     else {
-        var newLi = document.createElement('li');
-        newLi.setAttribute('class', 'list-group-item active shadow');
-        var checked = document.createElement('i');
-        checked.setAttribute('class', 'fas fa-check-circle fa-lg');
-        var trashed = document.createElement('i');
-        trashed.setAttribute('class', 'fa-solid fa-trash fa-lg');
-        var expirespan = document.createElement('span');
-        var spanned = document.createElement('span');
-        spanned.innerText = "".concat(addInput.value);
-        spanned.setAttribute('class', 'content');
-        checked.addEventListener('click', checkedMark);
-        trashed.addEventListener('click', trashedF);
-        newLi.appendChild(checked);
-        newLi.appendChild(trashed);
-        newLi.appendChild(spanned);
-        actualList.appendChild(newLi);
-        // Calculations for the expiery
-        // const today = new Date()
-        // const stringtoday = `${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`
-        // console.log(stringtoday, datePicker.value)
-        // console.log(Date.parse(stringtoday) > Date.parse(datePicker.value))
-        //local storage
-        // type listItem = {
-        //   value: string
-        //   expiry: string
-        // }
-        // const listItemCollection: (listItem | null)[] = []
-        // listItemCollection.push({ value: addInput.value, expiry: datePicker.value })
-        // localStorage.setItem('tasks', JSON.stringify(listItemCollection))
-        reset();
-        setTracker();
+        if (datePicker.value.split('-').length < 3) {
+            throwError('Invalid Date Inputted!');
+        }
+        else {
+            if (!localStorage.tasks) {
+                localStorage.setItem('tasks', JSON.stringify([]));
+                var list = JSON.parse(localStorage.getItem('tasks'));
+                list.push({
+                    value: addInput.value,
+                    expiry: datePicker.value,
+                    completed: false,
+                });
+                localStorage.setItem('tasks', JSON.stringify(list));
+            }
+            else {
+                var list = JSON.parse(localStorage.getItem('tasks'));
+                list.push({
+                    value: addInput.value,
+                    expiry: datePicker.value,
+                    completed: false,
+                });
+                localStorage.setItem('tasks', JSON.stringify(list));
+            }
+        }
     }
+    reset();
+    setTracker();
+    displayItems();
 }
 function checkedMark(e) {
     if (e.target instanceof Element) {
         var parent_1 = e.target.parentElement;
+        var tasks_1 = JSON.parse(localStorage.getItem('tasks'));
         if (parent_1.classList.contains('active')) {
             parent_1.classList.remove('active');
             parent_1.classList.add('inactive');
+            tasks_1.forEach(function (task) {
+                if (parent_1.id === String(tasks_1.indexOf(task))) {
+                    task.completed = true;
+                }
+            });
         }
         else {
             parent_1.classList.remove('inactive');
             parent_1.classList.add('active');
+            tasks_1.forEach(function (task) {
+                if (parent_1.id === String(tasks_1.indexOf(task))) {
+                    task.completed = false;
+                }
+            });
         }
+        localStorage.setItem('tasks', JSON.stringify(tasks_1));
     }
     setTracker();
 }
@@ -147,6 +183,14 @@ function trashedF(e) {
     if (e.target instanceof Element) {
         var parent_2 = e.target.parentElement;
         actualList.removeChild(parent_2);
+        var tasks_2 = JSON.parse(localStorage.getItem('tasks'));
+        console.log(tasks_2);
+        tasks_2.forEach(function (task) {
+            if (parent_2.id === String(tasks_2.indexOf(task))) {
+                tasks_2.pop(tasks_2.indexOf(task));
+            }
+            localStorage.setItem('tasks', JSON.stringify(tasks_2));
+        });
+        setTracker();
     }
-    setTracker();
 }
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,19 @@
 //task tracker
 
-document.addEventListener('DOMContentLoaded', (e: Event) => getItems())
+document.addEventListener('DOMContentLoaded', (e: Event) => {
+  displayItems()
+})
+
+type listItem = {
+  value: string
+  expiry: string
+  completed: boolean
+}
 
 const trackerContent: HTMLParagraphElement =
   document.querySelector('#taskTracker')!
+
+const listItemCollection: (listItem | null)[] = []
 
 const addbutton: HTMLButtonElement = document.querySelector('#addTaskButton')!
 const addInput: HTMLInputElement = document.querySelector('#addTaskInput')!
@@ -12,40 +22,74 @@ const actualList: HTMLUListElement = document.querySelector('#TaskulParent')!
 const SomethingWentWrong: HTMLDivElement = document.querySelector(
   '#liveAlertPlaceholder'
 )
+const sortSelect: HTMLSelectElement = document.querySelector('#sort-select')
 const taskTracker: HTMLParagraphElement = document.querySelector('#taskTracker')
 taskTracker.textContent = `${getActiveTask()} / ${getTasks()} Completed!`
 
-// //I guess this code is useful if you have like data from a jsonfile or a
-// //local storage but tbh if you're just creating everything from scratch this isn't
-// //even needed
-// const checkButton: NodeListOf<HTMLLIElement> =
-//   document.querySelectorAll('.fa-check-circle')
-// const trashButton: NodeListOf<HTMLLIElement> =
-//   document.querySelectorAll('.fa-trash ')
-
-// checkButton.forEach((btn) => {
-//   btn.addEventListener('click', checkedMark)
-// })
-// trashButton.forEach((btn) => {
-//   btn.addEventListener('click', trashedF)
-// })
-// //up to here
+sortSelect.addEventListener('change', sortit)
 
 addbutton.addEventListener('click', addTask)
 
 function reset(): void {
   addInput.value = ''
-  // console.log(checkButton)
+  datePicker.value = ''
 }
 
-function getItems(): void {
-  // the display function
-  const tasks = JSON.parse(localStorage.getItem('tasks'))
-  console.log(tasks)
+function sortit(): void {
+  const items = document.querySelectorAll('.list-group-item')
+  if (sortSelect.value === 'completed') {
+    items.forEach((item) => {
+      if (item.classList.contains('active')) {
+        item.classList.add('hide')
+      } else {
+        item.classList.remove('hide')
+      }
+    })
+  } else if (sortSelect.value === 'pending') {
+    items.forEach((item) => {
+      if (item.classList.contains('inactive')) {
+        item.classList.add('hide')
+      } else {
+        item.classList.remove('hide')
+      }
+    })
+  } else {
+    items.forEach((item) => {
+      if (item.classList.contains('hide')) {
+        item.classList.remove('hide')
+      }
+    })
+  }
+  setTracker()
+}
 
+function compareDates(date: string): string {
+  const compareDate = new Date(date)
+  const current = new Date()
+  if (compareDate < current) {
+    return 'expired '
+  } else {
+    return 'nonexpired'
+  }
+}
+
+function displayItems(): void {
+  actualList.innerHTML = ''
+  const tasks = JSON.parse(localStorage.getItem('tasks'))
+
+  tasks.sort((a: listItem, b: listItem) => a.value.localeCompare(b.value))
+  console.log(tasks)
   tasks.forEach((task) => {
     const newLi = document.createElement('li')
-    newLi.setAttribute('class', 'list-group-item active shadow')
+    newLi.setAttribute('class', 'list-group-item shadow')
+    if (task.completed) {
+      newLi.classList.add('inactive')
+    } else {
+      newLi.classList.add('active')
+    }
+
+    newLi.setAttribute('id', `${tasks.indexOf(task)}`)
+    newLi.setAttribute('name', `${task.value}`)
     const checked = document.createElement('i')
     checked.setAttribute('class', 'fas fa-check-circle fa-lg')
     const trashed = document.createElement('i')
@@ -54,36 +98,19 @@ function getItems(): void {
     const spanned = document.createElement('span')
     spanned.innerText = `${task.value}`
     spanned.setAttribute('class', 'content')
+    expirespan.innerText = ` Due ${task.expiry}`
+
+    expirespan.setAttribute('class', compareDates(task.expiry))
+
     checked.addEventListener('click', checkedMark)
     trashed.addEventListener('click', trashedF)
     newLi.appendChild(checked)
     newLi.appendChild(trashed)
+    newLi.appendChild(expirespan)
     newLi.appendChild(spanned)
-
     actualList.appendChild(newLi)
   })
-
-  // const newLi = document.createElement('li')
-  // newLi.setAttribute('class', 'list-group-item')
-  // //list-group-item active shadow"
-  // newLi.classList.add('active')
-  // newLi.classList.add('shadow')
-  // const checked = document.createElement('i')
-  // checked.setAttribute('class', 'fas fa-check-circle fa-lg')
-  // const trashed = document.createElement('i')
-  // trashed.setAttribute('class', 'fa-solid fa-trash fa-lg')
-  // const spanned = document.createElement('span')
-  // spanned.innerText = `${addInput.value}`
-  // spanned.setAttribute('class', 'content')
-  // checked.addEventListener('click', checkedMark)
-  // trashed.addEventListener('click', trashedF)
-  // newLi.appendChild(checked)
-  // newLi.appendChild(trashed)
-  // newLi.appendChild(spanned)
-
-  // actualList.appendChild(newLi)
 }
-
 function getTasks(): number {
   let count = 0
   document.querySelectorAll('.list-group-item').forEach((e) => {
@@ -100,69 +127,75 @@ function getActiveTask(): number {
   return count
 }
 
+function throwError(error: string): void {
+  SomethingWentWrong.innerText = error
+  SomethingWentWrong.style.display = 'block'
+  setTimeout((e: Event) => {
+    SomethingWentWrong.style.display = 'none'
+  }, 2000)
+}
+
 function setTracker(): void {
   taskTracker.textContent = `${getActiveTask()} / ${getTasks()} Completed!`
-  // console.log(actualList.children)
 }
 
 function addTask(e: Event): void {
   e.preventDefault()
-  if (addInput.value.length < 1) {
-    SomethingWentWrong.innerText = 'Invalid String Input!'
-    SomethingWentWrong.style.display = 'block'
-    setTimeout((e: Event) => {
-      SomethingWentWrong.style.display = 'none'
-    }, 2000)
+
+  if (addInput.value.trim().length < 1) {
+    throwError('Invalid String input')
   } else {
-    const newLi = document.createElement('li')
-    newLi.setAttribute('class', 'list-group-item active shadow')
-    const checked = document.createElement('i')
-    checked.setAttribute('class', 'fas fa-check-circle fa-lg')
-    const trashed = document.createElement('i')
-    trashed.setAttribute('class', 'fa-solid fa-trash fa-lg')
-    const expirespan = document.createElement('span')
-    const spanned = document.createElement('span')
-    spanned.innerText = `${addInput.value}`
-    spanned.setAttribute('class', 'content')
-    checked.addEventListener('click', checkedMark)
-    trashed.addEventListener('click', trashedF)
-    newLi.appendChild(checked)
-    newLi.appendChild(trashed)
-    newLi.appendChild(spanned)
-
-    actualList.appendChild(newLi)
-    // Calculations for the expiery
-    // const today = new Date()
-    // const stringtoday = `${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`
-    // console.log(stringtoday, datePicker.value)
-    // console.log(Date.parse(stringtoday) > Date.parse(datePicker.value))
-
-    //local storage
-
-    // type listItem = {
-    //   value: string
-    //   expiry: string
-    // }
-    // const listItemCollection: (listItem | null)[] = []
-    // listItemCollection.push({ value: addInput.value, expiry: datePicker.value })
-
-    // localStorage.setItem('tasks', JSON.stringify(listItemCollection))
-
-    reset()
-    setTracker()
+    if (datePicker.value.split('-').length < 3) {
+      throwError('Invalid Date Inputted!')
+    } else {
+      if (!localStorage.tasks) {
+        localStorage.setItem('tasks', JSON.stringify([]))
+        const list = JSON.parse(localStorage.getItem('tasks'))
+        list.push({
+          value: addInput.value,
+          expiry: datePicker.value,
+          completed: false,
+        })
+        localStorage.setItem('tasks', JSON.stringify(list))
+      } else {
+        const list = JSON.parse(localStorage.getItem('tasks'))
+        list.push({
+          value: addInput.value,
+          expiry: datePicker.value,
+          completed: false,
+        })
+        localStorage.setItem('tasks', JSON.stringify(list))
+      }
+    }
   }
+  reset()
+  setTracker()
+  displayItems()
 }
 
 function checkedMark(e: Event) {
   if (e.target instanceof Element) {
     const parent = e.target.parentElement
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
     if (parent.classList.contains('active')) {
       parent.classList.remove('active')
       parent.classList.add('inactive')
+      tasks.forEach((task) => {
+        if (parent.id === String(tasks.indexOf(task))) {
+          task.completed = true
+        }
+      })
     } else {
       parent.classList.remove('inactive')
       parent.classList.add('active')
+      tasks.forEach((task) => {
+        if (parent.id === String(tasks.indexOf(task))) {
+          task.completed = false
+        }
+      })
     }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
   }
   setTracker()
 }
@@ -171,7 +204,16 @@ function trashedF(e: Event) {
   if (e.target instanceof Element) {
     const parent = e.target.parentElement
     actualList.removeChild(parent)
-  }
+    const tasks = JSON.parse(localStorage.getItem('tasks'))
+    console.log(tasks)
 
-  setTracker()
+    tasks.forEach((task) => {
+      if (parent.id === String(tasks.indexOf(task))) {
+        tasks.pop(tasks.indexOf(task))
+      }
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    })
+
+    setTracker()
+  }
 }
